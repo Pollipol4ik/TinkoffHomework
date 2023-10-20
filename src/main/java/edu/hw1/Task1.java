55a2c1636a005023142572755078fbd957b6935e
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-10-20 22:28:50 +0300

    ready hw with 9

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,32 +1,29 @@
 package edu.hw1;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Task1 {
     private static final int SECONDS_IN_MINUTE = 60;
-    private static final Pattern NOT_NEGATIVE = Pattern.compile("^[0-9]*$");
 
     protected Task1() {
 
     }
 
     public static int getVideoLengthInSeconds(String videoLength) {
-        String[] timeArray = videoLength.split(":");
-        int minutes = Integer.parseInt(timeArray[0]);
-        int seconds = Integer.parseInt(timeArray[1]);
-        if (!isValid(timeArray[0]) || !isValid(timeArray[1])) {
-            return -1;
+        if (videoLength != null) {
+            String[] timeArray = videoLength.split(":");
+            if (timeArray[0].length() > 1 && timeArray[1].length() == 2 && timeArray.length == 2) {
+                try {
+                    int minutes = Integer.parseInt(timeArray[0]);
+                    int seconds = Integer.parseInt(timeArray[1]);
+                    if (seconds < SECONDS_IN_MINUTE && seconds >= 0 && minutes >= 0) {
+                        return minutes * SECONDS_IN_MINUTE + seconds;
+                    }
+                } catch (NumberFormatException e) {
+                    return -1;
+                }
+            }
         }
-        if (seconds >= SECONDS_IN_MINUTE || minutes < 0 || timeArray.length != 2) {
-            return -1;
-        }
-        return minutes * SECONDS_IN_MINUTE + seconds;
-    }
-
-    private static boolean isValid(String num) {
-        Matcher matcher = NOT_NEGATIVE.matcher(num);
-        return matcher.matches();
+        return -1;
     }
 }
 
