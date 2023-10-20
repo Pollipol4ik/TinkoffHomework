ca0d540461184a6e09ca47dee8d45d0f6c3d39fd
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-10-20 18:38:23 +0300

    ready hw with 2

--- src/test/java/edu/hw1/TestTask2.java
+++ src/test/java/edu/hw1/TestTask2.java
@@ -1,30 +1,26 @@
 package edu.hw1;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestTask2 {
-    @ParameterizedTest
-    @CsvSource(value = {
-        "0, 1",
-        "10, 2",
-        "544, 3",
-        "4666, 4",
-        "0000, 1",
-        "-22, 2",
-        "-123, 3",
-        "9, 1",
-        "-9, 1",
-        "-4356, 4",
-        "55555, 5",
-        "-55555, 5"
 
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1",
+        "5, 1",
+        "-5, 1",
+        "544, 3",
+        "-123, 3",
+        "4666, 4",
+        "2147483647, 10",
+        "-2147483648, 10"
     })
-    @DisplayName("Ввод чисел")
-    public void testCountDigits(int input, int ans) {
-        assertThat(Task2.countDigits(input)).isEqualTo(ans);
+    public void testCountDigits(int number, int expected) {
+        int result = Task2.countDigits(number);
+        assertEquals(expected, result);
     }
 }
+
 
