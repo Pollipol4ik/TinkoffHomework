e9ff3cf6b67a31389ceee4d88b7ff68b3c304518
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-11-05 19:30:14 +0300

    ready with tests2

--- src/test/java/edu/DFSTest.java
+++ src/test/java/edu/DFSTest.java
@@ -6,14 +6,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DFSTest {
-    @Test
-    @DisplayName("#generate test")
-    public void generate_shouldReturnMazeWhichHaveSolution() {
-        Maze testMaze = new DFSGenerator(11, 11).generate(11, 11);
-        List<Coordinate> actual =
-            new DepthFirstSolver(testMaze).solve(testMaze, new Coordinate(1, 1), new Coordinate(11, 11));
-        assertThat(actual).isNotEmpty();
-    }
+
     @Test
     @DisplayName("#generate test")
     public void generateHaveSolution() {
