ed51d54ff1aabcda976ba6d34985693ce6c4367d
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-12-03 23:42:54 +0300

    ready all 3

--- src/main/java/edu/task1/Client.java
+++ src/main/java/edu/task1/Client.java
@@ -24,20 +24,28 @@ public class Client {
     @SneakyThrows
     public void connect(@NonNull String messageToSend) {
         try (SocketChannel client = SocketChannel.open(hostAddress)) {
-            ByteBuffer buffer = ByteBuffer.wrap(messageToSend.getBytes(StandardCharsets.UTF_8));
-            while (buffer.hasRemaining()) {
-                client.write(buffer);
+
+            sendToServer(client, messageToSend);
+            receiveAndPrintResponse(client);
+        }
+    }
+
+    private void sendToServer(SocketChannel client, String message) throws Exception {
+        ByteBuffer buffer = ByteBuffer.wrap(message.getBytes(StandardCharsets.UTF_8));
+        while (buffer.hasRemaining()) {
+            client.write(buffer);
+        }
+    }
+
+    private void receiveAndPrintResponse(SocketChannel client) throws Exception {
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        int bytesRead;
+        while ((bytesRead = client.read(buffer)) != -1) {
+            if (bytesRead == 0) {
+                continue;
             }
-            buffer.flip();
-            buffer = ByteBuffer.allocate(BUFFER_SIZE);
-            int bytesRead;
-            while ((bytesRead = client.read(buffer)) != -1) {
-                if (bytesRead == 0) {
-                    continue;
-                }
-                System.out.println(new String(buffer.array(), StandardCharsets.UTF_8));
-                break;
-            }
+            System.out.println(new String(buffer.array(), 0, bytesRead, StandardCharsets.UTF_8));
+            break;
         }
     }
 
@@ -46,3 +54,4 @@ public class Client {
         return CLIENT_MESSAGES.get(random.nextInt(CLIENT_MESSAGES.size()));
     }
 }
+
