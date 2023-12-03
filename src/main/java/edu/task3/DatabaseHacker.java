0d2f78c8e6d1787bdd59fdf254bb402ace9e3ede
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-12-03 21:48:17 +0300

    ready with all test , somewhere incorrect 1

---
+++ src/main/java/edu/task3/DatabaseHacker.java
@@ -0,0 +1,8 @@
+package edu.task3;
+
+import java.util.Map;
+
+public interface DatabaseHacker {
+
+    Map<String, String> hack();
+}
---
+++ src/main/java/edu/task3/DefaultDatabaseHacker.java
@@ -0,0 +1,76 @@
+package edu.task3;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class DefaultDatabaseHacker extends AbstractDatabaseHacker {
+
+    private final Map<String, String> leakedDatabase;
+    private final Map<String, String> hackedData;
+
+    public DefaultDatabaseHacker(Map<String, String> leakedDatabase) {
+        this.leakedDatabase = leakedDatabase;
+        this.hackedData = new HashMap<>();
+    }
+
+    @Override
+    public Map<String, String> hack() {
+        while (!leakedDatabase.isEmpty()) {
+            for (int wordLength = MIN_PASSWORD_LENGTH; wordLength <= MAX_PASSWORD_LENGTH; wordLength++) {
+                generatePasswords(wordLength);
+            }
+        }
+        return hackedData;
+    }
+
+    private void generatePasswords(int wordLength) {
+        int[] index = new int[wordLength];
+        StringBuilder word = new StringBuilder();
+
+        Arrays.fill(index, 0);
+        while (!leakedDatabase.isEmpty()) {
+            word.setLength(0);
+
+            for (int value : index) {
+                word.append(alfCharacters.get(value));
+            }
+
+            String generatedPass = word.toString();
+            processGeneratedPassword(generatedPass);
+
+            if (!incrementIndex(index, alfCharacters.size())) {
+                break;
+            }
+        }
+    }
+
+    private boolean incrementIndex(int[] index, int maxValue) {
+        for (int i = index.length - 1; i >= 0; i--) {
+            index[i]++;
+            if (index[i] == maxValue) {
+                index[i] = 0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void processGeneratedPassword(String generatedPassword) {
+        String md5Pass = DigestUtils.md5Hex(generatedPassword);
+        if (isPasswordInLeakedDatabase(md5Pass)) {
+            addToHackedData(leakedDatabase.get(md5Pass), generatedPassword);
+            leakedDatabase.remove(md5Pass);
+        }
+    }
+
+    private boolean isPasswordInLeakedDatabase(String md5hex) {
+        return leakedDatabase.containsKey(md5hex);
+    }
+
+    private void addToHackedData(String username, String password) {
+        hackedData.put(username, password);
+    }
+}
