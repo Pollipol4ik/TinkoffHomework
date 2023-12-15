7d671a107df0ae429200d8a8619e16b271fe88ba
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-12-15 18:50:21 +0300

    with minor changes PortScannerTest

--- src/test/java/edu/PortScannerTest.java
+++ src/test/java/edu/PortScannerTest.java
@@ -1,14 +1,31 @@
 package edu;
 
 import edu.task6.PortScanner;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PortScannerTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
-    @DisplayName("printUsedPorts test")
-    public void printUsedPorts_shouldPrintListOfUsedPorts() {
-        assertDoesNotThrow(PortScanner::scanPorts);
+    void testScanPorts() {
+        PortScanner.scanPorts();
+        String output = outContent.toString();
+        assertTrue(output.contains("Протокол") && output.contains("Порт") && output.contains("Сервис"));
     }
 }
