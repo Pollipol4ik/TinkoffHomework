1f02d72c5d6cef3fa409ffdcbbe8de6e47fd19d2
Polina <your_email@whatever.comgit config --global user.name Polinagit config --global user.email your_email@whatever.com>
2023-12-15 18:36:37 +0300

    with test about FilterUtilsTest_4

--- src/main/java/edu/task3/AbstractFilter.java
+++ src/main/java/edu/task3/AbstractFilter.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 
-
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Path;
-
 @FunctionalInterface
 public interface AbstractFilter extends DirectoryStream.Filter<Path> {
     @Override
